dfdb940b4b46e7719d77e9ab649a2950ed8470b5
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-04-11 17:59:02 +0530

    Add files via upload

---
+++ 3.5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int superDigit(int num) {
+    if (num < 10) {
+        return num;
+    }
+    return superDigit(sumOfDigits(num));
+}
+
+int calculateSuperDigit(const string& n, int k) {
+    if (k <= 0) {
+        return -1;
+    }
+
+    int initialSum = 0;
+    for (char digit : n) {
+        if (!isdigit(digit)) {
+            return -1;
+        }
+        initialSum += digit - '0';
+    }
+
+    int multipliedSum = initialSum * k;
+
+    return superDigit(multipliedSum);
+}
+
+int main() {
+    string n;
+    int k;
+
+    cin >> n >> k;
+
+    int result = calculateSuperDigit(n, k);
+
+    if (result != -1) {
+        cout << result << endl;
+    }
+
+    return 0;
+}
