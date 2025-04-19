ddee1700c2a4aab927a6879f03f09d3d50318f82
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-04-19 20:53:36 +0530

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -1,35 +1,65 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int SumRecursive(int a[], int n){
-    if(n<=0)
-    return 0;
-    return (a[n-1] + SumRecursive(a, n-1));
-}
+class ArraySum
+{
+    int* arr;
+    int size;
 
-int SumIterative(int a[], int n){
-    int Sum=0;
-    for(int i=0; i<n; i++){
-        Sum+=a[i];
+    public:
+        ArraySum(int n)
+        {
+            size = n;
+            arr = new int[size];
+        }
+
+    ~ArraySum()
+    {
+        delete[] arr;
     }
-    return Sum;
-}
 
-int main()
+    void inputElements()
+    {
+        cout << "Enter elements in array:\n";
+        for (int i = 0; i < size; i++)
+        {
+            cin >> arr[i];
+        }
+    }
+
+    int sumRecursive(int n) 
+    {
+        if (n <= 0)
+            return 0;
+        return arr[n - 1] + sumRecursive(n - 1);
+    }
+
+    int sumIterative()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    void showSums() 
+    {
+        cout << "\nSum of all elements by recursive function: " << sumRecursive(size);
+        cout << "\nSum of all elements by iterative function: " << sumIterative();
+    }
+};
+
+int main() 
 {
     int n;
-    cout<<"\n\nEnter number of elements of integer array: ";
-    cin>>n;
-    int intArr[n];
+    cout << "\nEnter number of elements of integer array: ";
+    cin >> n;
 
-    cout<<"Enter elements in array: \n";
-    for(int i=0; i<n; i++){
-        cin>>intArr[i];
-    }
+    ArraySum obj(n);
+    obj.inputElements();
+    obj.showSums();
 
-    cout<<"\nSum of all elements by recursive function: "<<SumRecursive(intArr, n);
-    cout<<"\nSum of all elements by iterative function: "<<SumIterative(intArr, n);
-
-    return 0;
+    return 0;
 }
-
