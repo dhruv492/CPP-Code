e7488e4047769cace64bf2a90c612f5296800656
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-04-19 20:51:33 +0530

    Update 3.5.cpp

--- 3.5.cpp
+++ 3.5.cpp
@@ -1,51 +1,50 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int sumOfDigits(int num) {
-    int sum = 0;
-    while (num > 0) {
-        sum += num % 10;
-        num /= 10;
-    }
-    return sum;
-}
-
-int superDigit(int num) {
-    if (num < 10) {
-        return num;
-    }
-    return superDigit(sumOfDigits(num));
-}
-
-int calculateSuperDigit(const string& n, int k) {
-    if (k <= 0) {
-        return -1;
+class SuperDigitCalculator 
+{
+    int superDigit(int num)
+    {
+        if (num < 10)
+            return num;
+        return superDigit(sumOfDigits(num));
     }
 
-    int initialSum = 0;
-    for (char digit : n) {
-        if (!isdigit(digit)) {
-            return -1;
+    int sumOfDigits(int num) 
+    {
+        int sum = 0;
+        while (num > 0) 
+        {
+            sum += num % 10;
+            num /= 10;
         }
-        initialSum += digit - '0';
+        return sum;
     }
+    public:
+        int calculateSuperDigit(const string& n, int k) 
+        {
+            if (k <= 0)
+                return -1;
+            int initialSum = 0;
+            for (char digit : n)
+            {
+                if (!isdigit(digit)) 
+                    return -1;
+                initialSum += digit - '0';
+            }
+            int multipliedSum = initialSum * k;
+            return superDigit(multipliedSum);
+        }
+};
 
-    int multipliedSum = initialSum * k;
-
-    return superDigit(multipliedSum);
-}
-
-int main() {
+int main()
+{
     string n;
     int k;
-
     cin >> n >> k;
-
-    int result = calculateSuperDigit(n, k);
-
-    if (result != -1) {
-        cout << result << endl;
-    }
-
-    return 0;
+    SuperDigitCalculator calculator;
+    int result = calculator.calculateSuperDigit(n, k);
+    if (result != -1) cout << result << endl;
+    return 0;
 }
