d5a91e7246f9419737bfcc5f04a135549400c024
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 20:57:06 +0530

    Update 7.4.cpp

--- 7.4.cpp
+++ 7.4.cpp
@@ -5,42 +5,51 @@
 
 using namespace std;
 
-struct Student
+class StudentReport
 {
-    char name[50];
-    int marks;
-    char grade;
+private:
+    struct Student
+    {
+        char name[50];
+        int marks;
+        char grade;
+    };
+
+    const char* filename;
+
+    ostream& tab(ostream& output)
+    {
+        return output << setw(20) << left;
+    }
+
+public:
+    StudentReport(const char* file) : filename(file) {}
+
+    void generateReport()
+    {
+        ifstream file(filename, ios::binary);
+        if (!file)
+        {
+            cout << "Error: Unable to open file." << endl;
+            return;
+        }
+
+        Student student;
+        cout << tab << "Name" << tab << "Marks" << tab << "Grade" << endl;
+        cout << string(60, '-') << endl;
+
+        while (file.read(reinterpret_cast<char*>(&student), sizeof(Student)))
+        {
+            cout << tab << student.name << tab << student.marks << tab << student.grade << endl;
+        }
+
+        file.close();
+    }
 };
-
-ostream& tab(ostream& output)
-{
-    return output << setw(20) << left;
-}
-
-void generateReport(const char* filename)
-{
-    ifstream file(filename, ios::binary);
-    if (!file)
-    {
-        cout << "Error: Unable to open file." << endl;
-        return;
-    }
-
-    Student student;
-    cout << tab << "Name" << tab << "Marks" << tab << "Grade" << endl;
-    cout << string(60, '-') << endl;
-
-    while (file.read(reinterpret_cast<char*>(&student), sizeof(Student)))
-    {
-        cout << tab << student.name << tab << student.marks << tab << student.grade << endl;
-    }
-
-    file.close();
-}
 
 int main()
 {
-    const char* filename = "students.dat";
-    generateReport(filename);
+    StudentReport report("students.dat");
+    report.generateReport();
     return 0;
 }
