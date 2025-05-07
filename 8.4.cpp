6a169096b1260a72eb1f5ad12b12fef76f7812af
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 21:02:25 +0530

    Update 8.4.cpp

--- 8.4.cpp
+++ 8.4.cpp
@@ -4,30 +4,53 @@
 
 using namespace std;
 
-int main()
+class StudentSorter
 {
-    int n, score;
-    string name;
+private:
     vector<pair<string, int>> students;
 
-    cin >> n;
-
-    for (int i = 0; i < n; ++i)
+public:
+    void inputStudents()
     {
-        cin >> name >> score;
-        students.push_back({name, score});
+        int n;
+        cout << "Enter the number of students: ";
+        cin >> n;
+
+        string name;
+        int score;
+
+        cout << "Enter the names and scores:\n";
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> name >> score;
+            students.emplace_back(name, score);
+        }
     }
 
-    sort(students.begin(), students.end(), [](pair<string, int> a, pair<string, int> b)
+    void sortStudents()
     {
-        return a.second > b.second;
-    });
-
-    for (auto it = students.begin(); it != students.end(); ++it)
-    {
-        cout << it->first << " " << it->second << endl;
+        sort(students.begin(), students.end(), [](const pair<string, int>& a, const pair<string, int>& b)
+        {
+            return a.second > b.second;
+        });
     }
 
+    void displayStudents() const
+    {
+        cout << "\nSorted Students by Score:\n";
+        for (const auto& student : students)
+        {
+            cout << student.first << " " << student.second << endl;
+        }
+    }
+};
+
+int main()
+{
+    StudentSorter sorter;
+    sorter.inputStudents();
+    sorter.sortStudents();
+    sorter.displayStudents();
 
     return 0;
 }
