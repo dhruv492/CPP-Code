fb8fa0ed5e027933dea10b1e45fbbd8a3e05e5a7
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 20:57:49 +0530

    Update 7.5.cpp

--- 7.5.cpp
+++ 7.5.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+
 using namespace std;
 
-ostream& currency(ostream& os)
+class StudentFeeReport
 {
-    return os << "₹";
-}
+private:
+    struct Student
+    {
+        string name;
+        float marks;
+    };
+
+    Student students[3] = {
+        {"John Doe", 88.75},
+        {"Jane Smith", 92.50},
+        {"Samuel Lee", 78.90}
+    };
+
+    float tuition_fee;
+
+    ostream& currency(ostream& os) const
+    {
+        return os << "₹";
+    }
+
+public:
+    StudentFeeReport(float fee) : tuition_fee(fee) {}
+
+    void generateReport() const
+    {
+        cout << left << setw(20) << "Student Name"
+             << setw(10) << "Marks"
+             << setw(15) << "Tuition Fee" << endl;
+
+        cout << setfill('-') << setw(45) << "" << endl;
+        cout << setfill(' ');
+
+        for (const auto& student : students)
+        {
+            cout << left << setw(20) << student.name << setw(10) << fixed << setprecision(2) << student.marks
+                 << currency << setw(10) << fixed << setprecision(2) << tuition_fee << endl;
+        }
+    }
+};
 
 int main()
 {
-    string names[] = {"John Doe", "Jane Smith", "Samuel Lee"};
-    float marks[] = {88.75, 92.50, 78.90};
-    float tuition_fee = 35000.50;
-
-    cout << left << setw(20) << "Student Name"
-         << setw(10) << "Marks"
-         << setw(15) << "Tuition Fee" << endl;
-
-    cout << setfill('-') << setw(45) << "" << endl;
-    cout << setfill(' ');
-
-    for (int i = 0; i < 3; ++i)
-    {
-        cout << left << setw(20) << names[i] << setw(10) << fixed << setprecision(2) << marks[i]
-             << currency << setw(10) << fixed << setprecision(2) << tuition_fee << endl;
-    }
-
-
+    StudentFeeReport report(35000.50);
+    report.generateReport();
     return 0;
 }
