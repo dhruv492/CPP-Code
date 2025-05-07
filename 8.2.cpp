603f341fe6f173c4a1025eaf0d369b5bf8dd7c94
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 21:00:24 +0530

    Update 8.2.cpp

--- 8.2.cpp
+++ 8.2.cpp
@@ -5,24 +5,41 @@
 
 using namespace std;
 
+class WordFrequencyAnalyzer
+{
+private:
+    map<string, int> wordFrequency;
+
+public:
+    void processText(const string& text)
+    {
+        stringstream ss(text);
+        string word;
+
+        while (ss >> word)
+        {
+            ++wordFrequency[word];
+        }
+    }
+
+    void displayFrequency() const
+    {
+        for (const pair<const string, int>& entry : wordFrequency)
+        {
+            cout << entry.first << ": " << entry.second << endl;
+        }
+    }
+};
+
 int main()
 {
     string text;
+    cout << "Enter text: ";
     getline(cin, text);
 
-    map<string, int> wordFrequency;
-    stringstream ss(text);
-    string word;
-
-    while (ss >> word)
-    {
-        ++wordFrequency[word];
-    }
-
-    for (const pair<const string, int>& entry : wordFrequency)
-    {
-        cout << entry.first << ": " << entry.second << endl;
-    }
+    WordFrequencyAnalyzer analyzer;
+    analyzer.processText(text);
+    analyzer.displayFrequency();
 
     return 0;
 }
