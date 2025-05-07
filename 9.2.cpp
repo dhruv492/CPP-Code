efa852062789bffa1fe2303e749782031eb1b501
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 20:16:53 +0530

    Update 9.2.cpp

--- 9.2.cpp
+++ 9.2.cpp
@@ -3,13 +3,14 @@
 
 using namespace std;
 
-struct Transaction {
+struct Transaction 
+{
     string type;
     double amount;
 };
 
-class BankAccount {
-private:
+class BankAccount 
+{
     double balance;
     Transaction transactionLog[100];
     int transactionCount;
@@ -17,52 +18,67 @@ private:
 public:
     BankAccount() : balance(0), transactionCount(0) {}
 
-    void deposit(double amount) {
-        if (amount > 0) {
+    void deposit(double amount) 
+    {
+        if (amount > 0) 
+        {
             balance += amount;
             logTransaction("Deposit", amount);
             cout << "Deposited: " << amount << endl;
-        } else {
+        } 
+        else 
+        {
             handleError("Invalid deposit amount.");
         }
     }
 
-    void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
+    void withdraw(double amount) 
+    {
+        if (amount > 0 && amount <= balance) 
+        {
             balance -= amount;
             logTransaction("Withdraw", amount);
             cout << "Withdrew: " << amount << endl;
-        } else {
+        }
+        else 
+        {
             handleError("Invalid withdrawal amount.");
         }
     }
 
-    double getBalance() const {
+    double getBalance() const 
+    {
         return balance;
     }
 
-    void printTransactionLog() const {
-        for (int i = 0; i < transactionCount; i++) {
+    void printTransactionLog() const 
+    {
+        for (int i = 0; i < transactionCount; i++) 
+        {
             cout << transactionLog[i].type << ": " << transactionLog[i].amount << endl;
         }
     }
 
 private:
-    void logTransaction(const string& type, double amount) {
-        if (transactionCount < 100) {
+    void logTransaction(const string& type, double amount) 
+    {
+        if (transactionCount < 100) 
+        {
             transactionLog[transactionCount].type = type;
             transactionLog[transactionCount].amount = amount;
             transactionCount++;
         }
     }
 
-    void handleError(const string& message) {
+    void handleError(const string& message) 
+    {
         cout << message << endl;
         logTransaction("Error", 0);
     }
 };
 
-int main() {
+int main() 
+{
     BankAccount account;
     account.deposit(500);
     account.withdraw(200);
@@ -70,7 +86,6 @@ int main() {
     account.printTransactionLog();
 
     cout << "Current Balance: " << account.getBalance() << endl;
-    cout << "\n24AIML029_Raj\n";
 
     return 0;
 }
