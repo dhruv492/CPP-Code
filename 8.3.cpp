396db22e79f75369cf9e9768c4bbe91d8b331ce4
dhruv492 <dhruvchiragpatel0805@gmail.com>
2025-05-07 21:01:15 +0530

    Update 8.3.cpp

--- 8.3.cpp
+++ 8.3.cpp
@@ -3,23 +3,42 @@
 
 using namespace std;
 
+class UniqueNumberSet
+{
+private:
+    set<int> numbers;
+
+public:
+    void inputNumbers()
+    {
+        int n, x;
+        cout << "Enter the number of integers in the set: ";
+        cin >> n;
+
+        cout << "Enter " << n << " integers: ";
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> x;
+            numbers.insert(x);
+        }
+    }
+
+    void displayNumbers() const
+    {
+        cout << "Unique Sorted Numbers: ";
+        for (const auto& num : numbers)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+};
+
 int main()
 {
-    int n, x;
-    set<int> s;
-
-    cin >> n;
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> x;
-        s.insert(x);
-    }
-
-    for (const auto& num : s)
-    {
-        cout << num << " ";
-    }
-
+    UniqueNumberSet uniqueSet;
+    uniqueSet.inputNumbers();
+    uniqueSet.displayNumbers();
 
     return 0;
 }
